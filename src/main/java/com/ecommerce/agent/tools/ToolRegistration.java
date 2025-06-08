06b603ab1dc0d88f65147266b679653b9ddfd9ef
lifenghe-code <li_fenghe@outlook.com>
2025-06-08 16:22:51 +0800

    工具整合

---
+++ src/main/java/com/ecommerce/agent/tools/ToolRegistration.java
@@ -0,0 +1,36 @@
+package com.ecommerce.agent.tools;
+
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbacks;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ToolRegistration {
+//    @Value("${search-api.api-key}")
+//    private String searchApiKey;
+    @Bean
+    public ToolCallback[] allTools() {
+//        FileOperationTool fileOperationTool = new FileOperationTool();
+//        WebSearchTool webSearchTool = new WebSearchTool(searchApiKey);
+//        TerminalOperationTool terminalOperationTool = new TerminalOperationTool();
+//        WebScrapingTool webScrapingTool = new WebScrapingTool();
+        ResourceDownloadTool resourceDownloadTool = new ResourceDownloadTool();
+        PDFGenerationTool pdfGenerationTool = new PDFGenerationTool();
+        DateTimeTool dateTimeTool = new DateTimeTool();
+        TerminateTool terminateTool = new TerminateTool();
+        ExcelAnalysisTool excelAnalysisTool = new ExcelAnalysisTool();
+        ExchangeTool exchangeTool = new ExchangeTool();
+        ReturnVoucherTool returnVoucherTool = new ReturnVoucherTool();
+        return ToolCallbacks.from(
+                resourceDownloadTool,
+                dateTimeTool,
+                terminateTool,
+                excelAnalysisTool,
+                pdfGenerationTool,
+                exchangeTool,
+                returnVoucherTool
+        );
+    }
+}
