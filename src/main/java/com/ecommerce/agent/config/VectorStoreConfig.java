4f65dca8dc457a402fea605f1d3cf7e1eef49fb7
lifenghe-code <li_fenghe@outlook.com>
2025-06-08 16:28:38 +0800

    Vector Store配置文件

---
+++ src/main/java/com/ecommerce/agent/config/VectorStoreConfig.java
@@ -0,0 +1,31 @@
+package com.ecommerce.agent.config;
+
+import com.ecommerce.agent.rag.MyDocumentLoader;
+import jakarta.annotation.Resource;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+// @Configuration
+public class VectorStoreConfig {
+
+    @Resource
+    private MyDocumentLoader myDocumentLoader;
+
+
+    @Bean
+    VectorStore customerServiceAppVectorStore(EmbeddingModel dashscopeEmbeddingModel) {
+        SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(dashscopeEmbeddingModel)
+                .build();
+        // 加载文档
+        List<Document> documents = myDocumentLoader.loadMarkdowns();
+        simpleVectorStore.add(documents);
+        return simpleVectorStore;
+    }
+}
+
